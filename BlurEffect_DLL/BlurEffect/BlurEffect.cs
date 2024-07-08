e1073ed48e65664fc8ba043e41ff039d08aadd9c
Kaminasta <72865618+Kaminasta@users.noreply.github.com>
2024-07-08 18:27:15 +0500

    Add files via upload

---
+++ BlurEffect_DLL/BlurEffect/BlurEffect.cs
@@ -0,0 +1,101 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace BlurEffect
+{
+    internal enum AccentState
+    {
+        ACCENT_DISABLED = 0,
+        ACCENT_ENABLE_GRADIENT = 1,
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+        ACCENT_ENABLE_BLURBEHIND = 3,
+        ACCENT_INVALID_STATE = 4
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public int GradientColor;
+        public int AnimationId;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+
+    internal enum WindowCompositionAttribute {
+        WCA_ACCENT_POLICY = 19
+    }
+    public static class WindowExtensions
+    {
+        [DllImport("user32.dll")]
+        private static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        /// <summary>
+        /// Включает эффект размытия для окна.
+        /// </summary>
+        /// <param name="window">Окно, для которого необходимо включить размытие.</param>
+        /// <remarks>
+        /// Учтите что данный метод использует API Windows для эффекта размытия.
+        /// </remarks>
+        public static void EnableBlur(this Window window)
+        {
+            var windowHelper = new WindowInteropHelper(window);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        /// <summary>
+        /// Отключает эффект размытия для окна.
+        /// </summary>
+        /// <param name="window">Окно, для которого необходимо отключить размытие.</param>
+        /// <remarks>
+        /// Этот метод использует API Windows для отключения эффекта размытия.
+        /// </remarks>
+        public static void DisableBlur(this Window window)
+        {
+            var windowHelper = new WindowInteropHelper(window);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_DISABLED; 
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        
+    }
+}
---
+++ WpfApp_Example/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using BlurEffect;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void BlurOn_Click(object sender, RoutedEventArgs e)
+        {
+            this.EnableBlur();
+        }
+
+        private void BlurOff_Click(object sender, RoutedEventArgs e)
+        {
+            this.DisableBlur();
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OpenWeb_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/Kaminasta/BlurEffect_WPF",
+                UseShellExecute = true
+            });
+        }
+    }
+}
